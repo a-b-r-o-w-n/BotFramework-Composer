021e131e443bea203b9b8387fa27a45454301f12
TJ Durnford <tjdford@gmail.com>
2020-02-20 09:29:19 -0800

    Added tests

--- Composer/jest.config.js
+++ Composer/jest.config.js
@@ -37,6 +37,15 @@ module.exports = {
     '\\.(s)?css$': '<rootDir>/jestMocks/styleMock.js',
   },
   testPathIgnorePatterns: ['/node_modules/', '/scripts/', '/jestMocks/', '__tests__/setup.(j|t)s', '/cypress/'],
+
+  // Some node modules are packaged and distributed in a non-transpiled form
+  // (ex. contain import & export statements); and Jest won't be able to
+  // understand them because node_modules aren't transformed by default. So
+  // we can specify that they need to be transformed here.  // Some node modules are packaged and distributed in a non-transpiled form
+  // (ex. contain import & export statements); and Jest won't be able to
+  // understand them because node_modules aren't transformed by default. So
+  // we can specify that they need to be transformed here.
+  transformIgnorePatterns: ['"/node_modules/(?!office-ui-fabric-react).+\\.js$"'],
   projects: [
     '<rootDir>/packages/client',
     '<rootDir>/packages/server',
@@ -46,5 +55,6 @@ module.exports = {
     '<rootDir>/packages/lib/code-editor',
     '<rootDir>/packages/lib/shared',
     '<rootDir>/packages/tools/language-servers/language-generation',
+    '<rootDir>/packages/ui-plugins',
   ],
 };
