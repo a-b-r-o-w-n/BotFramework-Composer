6e85dc69a4c827c311655c1483061d7324440890
tdurnford <tjdford@gmail.com>
2020-03-04 19:24:03 -0800

    feat: Updated FormRow to support multiple fields in a row

--- Composer/packages/extensions/adaptive-form/src/components/FormRow.tsx
+++ Composer/packages/extensions/adaptive-form/src/components/FormRow.tsx
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
 import React from 'react';
 import { FieldProps, UIOptions } from '@bfc/extension';
 
@@ -13,24 +15,48 @@ interface FormRowProps extends Omit<FieldProps, 'onChange'> {
   row: string | [string, string];
 }
 
-const FormRow: React.FC<FormRowProps> = ({
-  id,
-  depth,
-  schema,
-  row,
-  definitions,
-  value,
-  uiOptions,
-  transparentBorder,
-  className,
-  label,
-  rawErrors,
-  onBlur,
-  onFocus,
-  onChange,
-}) => {
+const formRow = {
+  row: css`
+    display: flex;
+    margin: 10px 18px;
+    label: FormRowContainer;
+  `,
+  property: css`
+    flex: 1;
+    margin: 0;
+
+    & + & {
+      margin-left: 16px;
+    }
+  `,
+};
+
+const FormRow: React.FC<FormRowProps> = props => {
+  const {
+    id,
+    depth,
+    schema,
+    row,
+    definitions,
+    value,
+    uiOptions,
+    transparentBorder,
+    className,
+    label,
+    rawErrors,
+    onBlur,
+    onFocus,
+    onChange,
+  } = props;
+
   if (Array.isArray(row)) {
-    return <>Multi field row</>;
+    return (
+      <div css={formRow.row}>
+        {row.map((property, index) => (
+          <FormRow key={index} css={formRow.property} {...props} depth={depth + 1} row={property} />
+        ))}
+      </div>
+    );
   }
   const propSchema = resolvePropSchema(schema, row, definitions);
 
@@ -56,7 +82,7 @@ const FormRow: React.FC<FormRowProps> = ({
     );
   }
 
-  return <>Form Row</>;
+  return <React.Fragment>Form Row</React.Fragment>;
 };
 
 export { FormRow };
